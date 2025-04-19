843a47e892a2f8a816daa580f6aec2076de1dea2
ryan4336 <rahhhp@umsystem.edu>
2025-04-19 12:31:11 -0500

    Row class implemented to pass all test cases

--- src/hw4/maze/Row.java
+++ src/hw4/maze/Row.java
@@ -1,5 +1,33 @@
 package hw4.maze;
 
-public class Row {
+import java.util.ArrayList;
 
+public class Row {
+	ArrayList<Cell> cells;
+
+	public Row(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+	
+	public Cell getCell(int i) {
+		return cells.get(i);
+	}
+
+	@Override
+	public String toString() {
+		return "Row [cells=" + cells + "]";
+	}
+	
+	
+	
 }
+
+	
