85e8c0bddb8c54beb53d3be10e70005591521b64
ryan4336 <rahhhp@umsystem.edu>
2025-04-19 12:35:08 -0500

    Grid class implemented to pass all test methods

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -1,5 +1,26 @@
 package hw4.maze;
 
-public class Grid {
+import java.util.ArrayList;
 
+public class Grid {
+	ArrayList<Row> rows;
+
+	public Grid(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+
+	public ArrayList<Row> getRows() {
+		return rows;
+	}
+
+	public void setRows(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
+	
+	
 }
