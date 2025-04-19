6777067ca768c879f8f7b61de69dd3757cd5219f
ryan4336 <rahhhp@umsystem.edu>
2025-04-19 12:44:44 -0500

    Implemented Player class that passes all test cases

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -1,5 +1,31 @@
 package hw4.player;
 
-public class Player {
+import hw4.maze.Cell;
+import hw4.maze.Row;
 
+public class Player {
+	
+	Cell currentCell;
+	Row currentRow;
+	
+	public Player(Row row, Cell cell) {
+		this.currentRow = row;
+		this.currentCell = cell;
+	}
+
+	public Cell getCurrentCell() {
+		return currentCell;
+	}
+
+	public Row getCurrentRow() {
+		return currentRow;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
+	}
+	
+	
+	
 }
