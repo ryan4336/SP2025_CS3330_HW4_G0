20fdfb59fb53cdff5619bef689776b442cb79303
ryan4336 <rahhhp@umsystem.edu>
2025-04-22 15:46:31 -0500

    added java doc to hw4.player package

--- src/hw4/player/Movement.java
+++ src/hw4/player/Movement.java
@@ -1,9 +1,24 @@
 package hw4.player;
-
+/**
+ * Defines the possible movement directions in the game.
+ * These directions represent the ways the player can navigate through the maze.
+ */
 public enum Movement {
+	/**
+     * Represents moving up in the grid.
+     */
 	UP, 
+	/**
+     * Represents moving right in the grid.
+     */
 	RIGHT, 
+	/**
+     * Represents moving down in the grid.
+     */
 	DOWN, 
+	/**
+     * Represents moving left in the grid.
+     */
 	LEFT
 
 }
--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -2,34 +2,62 @@ package hw4.player;
 
 import hw4.maze.Cell;
 import hw4.maze.Row;
-
+/**
+ * Represents a player in the game.
+ * The player is associated with a specific cell and row in the grid.
+ */
 public class Player {
 
 	private Cell currentCell;
 	private Row currentRow;
-
+	/**
+	 * Constructs a Player with a specified row and cell.
+	 *
+	 * @param row the row the player is currently in
+	 * @param cell the cell the player is currently occupying
+	 */
 	public Player(Row row, Cell cell) {
 		this.currentRow = row;
 		this.currentCell = cell;
 	}
-
+	/**
+	 * Returns the cell the player is currently in.
+	 *
+	 * @return the current Cell object
+	 */
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
-
+	/**
+	 * Returns the row the player is currently in.
+	 *
+	 * @return the current Row object
+	 */
 	public Row getCurrentRow() {
 		return currentRow;
 	}
-
+	/**
+	 * Returns a string representation of the player.
+	 *
+	 * @return a string describing the player's current cell and row
+	 */
 	@Override
 	public String toString() {
 		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
 	}
-
+	/**
+	 * Sets the cell the player is currently in.
+	 *
+	 * @param currentCell the new cell to set for the player
+	 */
 	public void setCurrentCell(Cell currentCell) {
 		this.currentCell = currentCell;
 	}
-
+	/**
+	 * Sets the row the player is currently in.
+	 *
+	 * @param currentRow the new row to set for the player
+	 */
 	public void setCurrentRow(Row currentRow) {
 		this.currentRow = currentRow;
 	}
